8864d5f58a8a7c9313c091dcab1d6f71c7ec0d31
pkj <1417752158@qq.com>
2020-09-08 20:16:04 +0800

    Update 1.cpp

--- CSP/201909/1.cpp
+++ CSP/201909/1.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<cstdio>
 #include<cmath>
 
 using namespace std;
@@ -8,18 +8,18 @@ int main() {
     int sum = 0;
     int largest_id=0,largest_num=0;
 
-    cin>>N>>M;
+    scanf("%d%d",&N,&M);
 
     int init_num;
     for (int i = 1; i <= N; i++)
     {
-        cin>>init_num;
+        scanf("%d",&init_num);
         sum += init_num;
 
         int drop_num,total_num=0;
         for (int j = 0; j < M; ++j)
         {
-            cin>>drop_num;
+            scanf("%d",&drop_num);
             total_num += abs(drop_num);
         }
 
@@ -27,6 +27,6 @@ int main() {
         largest_id = (total_num>largest_num)?i:largest_id;
         largest_num = (total_num>largest_num)?total_num:largest_num;
     }
-    cout<<sum<<" "<<largest_id<<" "<<largest_num<<endl;
+    printf("%d %d %d\n",sum,largest_id,largest_num);
     return 0;
 }
